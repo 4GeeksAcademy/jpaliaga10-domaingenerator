84e52c2a062368515e268878e536875d118c4aab
jpaliaga10 <155824170+jpaliaga10@users.noreply.github.com>
2024-03-23 11:37:05 +0000

    exercise generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+        let domaines = pronoun[i] + adj[j] + noun[k] + ".net";
+        console.log(domain);
+        console.log(domaines);
+      }
+    }
+  }
+
+  let all = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        all.push(pronoun[i] + adj[j] + noun[k] + ".es");
+      }
+    }
+  }
+  console.log(all);
 };
